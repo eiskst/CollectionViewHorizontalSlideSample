07a3d64f7dc0d039d31afa43996461517ab9e338
Eisuke Sato <eiskst@gmail.com>
2017-08-22 13:57:06 +0900

    Initial Commit

---
+++ SlideSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideSample
+//
+//  Created by Eisuke Sato on 2017/08/22.
+//  Copyright © 2017年 Eisuke Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
